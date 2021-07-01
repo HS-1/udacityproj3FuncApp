a2c91e0ceaf8381893a8c81f9e86795fbc5bdf99
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-07-01 16:07:53 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 adminEmail = "info@test.com"
 SECRET_KEY = "LWd2tzlprdGHCIPHTd4tp5SBFgDszm"
-SERVICE_BUS_CONNECTION_STRING ="Endpoint=sb://hsproj3servbus2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=9s5KVZO4ENUkyNZTPTvD70gu5UBNo0ZceZjEsAZHvkg=" #TODO: Update value
+SERVICE_BUS_CONNECTION_STRING ="Endpoint=sb://hsproj3servbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=b60eeDPUEHC30PrwHKgQzNArr7I8/NvFskXHzPEirsw=" #TODO: Update value
 SERVICE_BUS_QUEUE_NAME ="notificationqueue"
 ADMIN_EMAIL_ADDRESS: "info@techconf.com"
 SENDGRID_API_KEY = "" #Configuration not required, required SendGrid Account
