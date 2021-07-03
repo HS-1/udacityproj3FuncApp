228f6532146fcb6047d604d6cc7cae1e25368c95
HS-1 <66611834+HS-1@users.noreply.github.com>
2021-07-03 16:46:21 +0800

    Update __init__.py

--- ServiceBusQueueTrigger1/__init__.py
+++ ServiceBusQueueTrigger1/__init__.py
@@ -13,22 +13,22 @@ def main(msg: func.ServiceBusMessage):
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
     # TODO: Get connection to database
-    conn = psycopg2.connect("dbname='techconfdb' user='serverAdmin@hsproj3dbserver' host='hsproj3dbserver.postgres.database.azure.com' password='P@ssword' port='5432' sslmode='true'")
-    cursor = conn.cursor()
+    #conn = psycopg2.connect("dbname='techconfdb' user='serverAdmin@hsproj3dbserver' host='hsproj3dbserver.postgres.database.azure.com' password='P@ssword' port='5432' sslmode='true'")
+    #cursor = conn.cursor()
     try:
         # TODO: Get notification message and subject from database using the notification_id
         # Fetch all rows from table
-        cursor.execute("SELECT * FROM notification where id={};".format(notification_id))
-        rows = cursor.fetchall()
+        #cursor.execute("SELECT * FROM notification where id={};".format(notification_id))
+        #rows = cursor.fetchall()
 
         # Print all rows
-        for row in rows:
-            print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
+        #for row in rows:
+            #print("Data row = (%s, %s, %s)" %(str(row[0]), str(row[1]), str(row[2])))
             # TODO: Get attendees email and name
             # TODO: Loop through each attendee and send an email with a personalized subject
-            attendees = cursor.execute("SELECT * FROM attendee")
-            for attendee in attendees:
-                subject = '{}: {}'.format(attendee.first_name, row.subject)
+            #attendees = cursor.execute("SELECT * FROM attendee")
+            #for attendee in attendees:
+                #subject = '{}: {}'.format(attendee.first_name, row.subject)
                 #message = Mail(
                 #    from_email='in,
                 #    to_emails=attendee.email,
@@ -38,7 +38,7 @@ def main(msg: func.ServiceBusMessage):
                 #sg.send(message)
         # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
         date = datetime.utcnow()
-        status = 'Notified {} attendees'.format(len(attendees.details))
+        status = 'Notified {} attendees'.format(5)
         cursor.execute("UPDATE notification SET completed_date=%s, status=%s WHERE id=%s;", (date, status, notification_id))
         conn.commit()
 
